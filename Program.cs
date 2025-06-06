1b6a3b97a5877dc1d3d63e542a2f466337591e23
HwaHyang (KangHyeon Kim) <hwahyang@meowlabs.kr>
2025-06-06 20:10:18 +0900

    feat: client uid management

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Net.Sockets;
+using System.Collections.Concurrent;
 
 using Chess_Server.Modules;
 
@@ -17,6 +18,10 @@ namespace Chess_Server
 		                                                         };
 		
 		private static TcpListener listener;
+		private static ConcurrentDictionary<string, TcpClient> clientStreams = new ConcurrentDictionary<string, TcpClient>();
+		
+		private static readonly object uidLock = new object();
+		private static int uidCounter = 0;
 		
 		static void Main(string[] args)
 		{
@@ -64,6 +69,9 @@ namespace Chess_Server
 			byte[] buffer = new byte[Config.BUFFER_SIZE];
 			int byteCount;
 
+			string uid = GenerateUid();
+			clientStreams.TryAdd(uid, client);
+
 			try
 			{
 				while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
@@ -87,6 +95,65 @@ namespace Chess_Server
 				
 				stream.Close();
 				client.Close();
+
+				clientStreams.TryRemove(uid, out TcpClient _);
+			}
+		}
+
+		/// <summary>
+		/// 특정 클라이언트에 데이터를 전송합니다.
+		/// </summary>
+		/// <param name="uid">전송할 클라이언트의 uid를 지정합니다.</param>
+		/// <param name="data">전송할 데이터를 지정합니다.</param>
+		private static void SendData(string uid, string data)
+		{
+			TcpClient? client = null;
+			NetworkStream? stream = null;
+			try
+			{
+				if (!clientStreams.ContainsKey(uid)) return;
+				client = clientStreams[uid];
+				if (!IsClientConnected(client)) return;
+				stream = client.GetStream();
+				
+				byte[] responseBytes = Encoding.UTF8.GetBytes(data);
+				stream.Write(responseBytes, 0, responseBytes.Length);
+				Console.WriteLine("[{0}] Sent: {1}", client.Client.RemoteEndPoint, data);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("[{0}] Client Error: {1}", client?.Client.RemoteEndPoint?.ToString() ?? uid, e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("[{0}] Client connection lost.", client?.Client.RemoteEndPoint?.ToString() ?? uid);
+				
+				stream?.Close();
+				client?.Close();
+			}
+		}
+		
+		private static string GenerateUid()
+		{
+			lock (uidLock)
+			{
+				return "u_" + Interlocked.Increment(ref uidCounter).ToString("D8");
+			}
+		}
+		
+		private static bool IsClientConnected(TcpClient client)
+		{
+			try
+			{
+				if (client.Client.Poll(0, SelectMode.SelectRead) && client.Client.Available == 0)
+				{
+					return false;
+				}
+				return true;
+			}
+			catch
+			{
+				return false;
 			}
 		}
 	}
